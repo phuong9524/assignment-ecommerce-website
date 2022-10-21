ab6f4b17ffcc6169141c9a4803d5cb6bcf0bf472
PhuongDo <phuong9524hth@gmail.com>
2022-10-21 11:37:34 +0700

    fix json name

--- backend/src/main/java/com/nashtech/backend/dto/request/ProductUpdateDto.java
+++ backend/src/main/java/com/nashtech/backend/dto/request/ProductUpdateDto.java
@@ -2,6 +2,9 @@ package com.nashtech.backend.dto.request;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.nashtech.backend.data.entities.ProductCategory;
+import com.nashtech.backend.data.entities.ProductInventory;
+import com.nashtech.backend.data.entities.Rating;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -23,15 +26,28 @@ public class ProductUpdateDto {
     @Min(value = 1, message = "Price should be a positive number")
     private Double price;
 
+    @NotBlank(message = "Product image is required")
+    private String image;
+
+    private ProductCategory productCategory;
+
+    private ProductInventory productInventory;
+
 
     @JsonCreator
     public ProductUpdateDto(
             @JsonProperty("name") String name,
             @JsonProperty("desc") String desc,
-            @JsonProperty("price") Double price) {
+            @JsonProperty("price") Double price,
+            @JsonProperty("image") String image,
+            @JsonProperty("productCategory") ProductCategory productCategory,
+            @JsonProperty("productInventory") ProductInventory productInventory) {
         this.name = name;
         this.desc = desc;
         this.price = price;
+        this.image = image;
+        this.productCategory = productCategory;
+        this.productInventory = productInventory;
     }
 
 
